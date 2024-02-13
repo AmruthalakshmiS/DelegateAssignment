45df8ce58d103f188a3ac812b96dc80895444b44
Amrutha Lakshmi S <lakshmisamrutha@gmail.com>
2024-02-14 01:30:01 +0530

    added quick sort algo

--- DelegateAssignment/Program.cs
+++ DelegateAssignment/Program.cs
@@ -1,2 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
+using DelegateAssignment;
+using DelegateAssignment.Models;
+
+
+public class Program
+{
+    private static readonly List<Student> _students = new()
+    {
+        new Student { Id = 100, Name = "Ram", Age = 15, Score = 99 },
+        new Student { Id = 121, Name = "Arjun", Age = 19, Score = 89.8 },
+        new Student { Id = 101, Name = "Rahul", Age = 15, Score = 99.9 },
+        new Student { Id = 102, Name = "Riya", Age = 16, Score = 78.5 }
+    };
+    public static void Main(string[] args)
+    {
+        bool start = true;
+        while (start)
+        {
+            Console.WriteLine("\nAvailable Menu:\n");
+            Console.WriteLine("1. Sort and Display Students");
+            Console.WriteLine("2. Add User");
+            Console.WriteLine("3. Update User");
+            Console.WriteLine("4. Remove User");
+            Console.WriteLine("5. Show Users");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine("Enter your choice: ");
+
+            switch(Console.ReadLine())
+            {
+                case "1":
+                       SortStudents();
+                        break;
+                case "2":
+                    //AddUser();
+                    break;
+                case "3":
+                    //UpdateUser();
+                    break;
+                case "4":
+                    //RemoveUser();
+                    break;
+                case "5":
+                    //ShowUser();
+                    break;
+                case "6":
+                    start = false;
+                    break;
+                default:
+                    Console.WriteLine("Choose from 1 to 6");
+                    break;
+
+
+            }
+
+        }
+
+    }
+
+    public static void SortStudents()
+    {
+        var students = _students.ToArray();
+        Sort.QuickSort(students, (x, y) => x.Score.CompareTo(y.Score));
+
+        Console.WriteLine("Sorted list of students in ascending order:\n");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"ID: {student.Id}, Name: {student.Name}, Age: {student.Age}, Score: {student.Score}");
+            //Console.WriteLine();
+        }
+
+    }
+}
--- DelegateAssignment/Sort.cs
+++ DelegateAssignment/Sort.cs
@@ -8,5 +8,47 @@ namespace DelegateAssignment
 {
     public class Sort
     {
+        public static void QuickSort<T>(T[] array, Comparison<T> comparison) 
+        {
+            QuickSortInternal(array, 0, array.Length - 1, comparison);
+        }
+
+        private static void QuickSortInternal<T>(T[] array, int left, int right, Comparison<T> comparison)
+        {
+            if (left >= right)
+            {
+                return;
+            }
+
+            int partition = PartitionInternal(array, left, right, comparison);
+
+            QuickSortInternal(array, left, partition - 1, comparison);
+            QuickSortInternal(array, partition + 1, right, comparison);
+        }
+
+        private static int PartitionInternal<T>(T[] list, int left, int right, Comparison<T> comparison) 
+        {
+            T partition = list[right];
+
+            // stack items smaller than partition from left to right
+            int swapIndex = left;
+            for (int i = left; i < right; i++)
+            {
+                T item = list[i];
+                if (comparison(item,partition) <= 0)
+                {
+                    list[i] = list[swapIndex];
+                    list[swapIndex] = item;
+
+                    swapIndex++;
+                }
+            }
+
+            // put the partition after all the smaller items
+            list[right] = list[swapIndex];
+            list[swapIndex] = partition;
+
+            return swapIndex;
+        }
     }
 }
