957e4881188829a2ee85b59abfd9290ca645a09d
Amrutha Lakshmi S <lakshmisamrutha@gmail.com>
2024-02-14 01:54:11 +0530

    user repository class added

---
+++ DelegateAssignment/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAssignment
+{
+    public class User
+    {
+        private static int _lastId = 0;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+
+        public User(string name, string email, string contact){
+            this.Id = ++_lastId;
+            this.Name = name;
+            this.Email = email;
+            this.Contact = contact;
+         }
+
+    }
+}
