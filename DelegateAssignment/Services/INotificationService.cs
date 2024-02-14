f589ec9790d932d9aea509b4aa36703faa704eac
Amrutha Lakshmi S <lakshmisamrutha@gmail.com>
2024-02-14 10:47:57 +0530

    notification service added

---
+++ DelegateAssignment/Services/INotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAssignment.Services
+{
+    public class INotificationService
+    {
+        void Notification(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ DelegateAssignment/Services/Notification.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAssignment.Services
+{
+    public class Notification : INotificationService
+    {
+        public class SmsService
+        {
+            public void Notify(string message)
+            {
+                Console.WriteLine($"SMS notification, {message}");
+            }
+        }
+
+        public class EmailService
+        {
+            public void Notify(string message) 
+            {
+                Console.WriteLine($"Email notification, {message}");
+            }
+        }
+
+        public class PushService
+        {
+            public void Notify(string message)
+            {
+                Console.WriteLine($"Email notification, {message}");
+            }
+        }
+
+
+    }
+}
